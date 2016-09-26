e9328bcfec6123233778a4bbb98348f3a0028a79
BYC810 <BYC@yuanchao.local>
2016-09-26 10:36:15 +0800

    创建工程

---
+++ BYCTVSHOW/BYCTVSHOW/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BYCTVSHOW
+//
+//  Created by 元朝 on 16/9/26.
+//  Copyright © 2016年 超哥直播. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
